0f7601ba78ff2d20c973a88aec9f961ff9b8bf10
Joyee Cheung <joyeec9h3@gmail.com>
2015-08-02 18:13:49 +0800

    format

--- public/js/jsonp.js
+++ public/js/jsonp.js
@@ -1,6 +1,14 @@
 function handleData(data) {
-  var pre = document.createElement('pre');
-  var text = JSON.stringify(data);
-  pre.appendChild(document.createTextNode(text));
-  document.body.appendChild(pre);
+  var pre = document.getElementById('get-jsonp-data');
+  var text = JSON.stringify(data, null, 4);
+  pre.textContent = text;
 }
+
+var getButton = document.getElementById('get-jsonp-button');
+
+getButton.addEventListener('click', function() {
+  var script = document.createElement("script");
+  var id = document.getElementById('get-jsonp-id').value;
+  script.src = "http://bar.com/api/jsonp/" + id + "?_cb=handleData"
+  document.head.appendChild(script);
+});
